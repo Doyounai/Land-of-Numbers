f3b0abf6447c47d096f84d4bc7ca63c34b95cc07
kong <peeradech_wongkum@cmu.ac.th>
2024-02-26 22:05:50 +0700

    add: eslint prettier

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,18 +1,28 @@
-import Rocket from '../objects/rocket';
+import Rocket from "../objects/rocket";
 
 export default class MainScene extends Phaser.Scene{
     rocket: Rocket;
 
     constructor(){
-        super({key: 'MainScene'});
+        super({key: "MainScene"});
     }
 
     create(){
         this.rocket =  new Rocket(this,
             this.cameras.main.width / 2,
             this.cameras.main.height / 2
-            );
+        );
     }
+
+    test = () => {
+        console.log("test");
+
+        const x = 2;
+        if(x > 1)
+        {
+            console.log();
+        }
+    };
 
     update(time: number, delta: number): void {
         if(this.rocket.body)
